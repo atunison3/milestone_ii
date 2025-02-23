0b7151bc8801c73c6fbdb2f6ac134b97f7b04471
atunison3 <atunison1@yahoo.com>
2025-02-23 13:19:26 -0600

    Updated supervised.py

--- team17/super/parameters.py
+++ team17/super/parameters.py
@@ -1,10 +1,10 @@
 # Select features
-num_features = ['start_soc', 'year']
+num_features = ['start_soc']
 cat_features = [
     'evse_id', 'connector_id', 'metro_area', 'land_use', 'num_ports', 'venue', 
     'pricing', 'month', 'day', 'hour', 'day_of_year', 'quarter', 'weekday', 'week_num'
     ]
 cat_features = [
-    'connector_id', 'metro_area', 
+    'connector_id', 'metro_area', 'land_use', 
     'pricing', 'month', 'day', 'hour', 'day_of_year', 'weekday'
     ]
